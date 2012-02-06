273290c2bf2849b448fa44e8b7b29caa68659026
Peter Rudolfsen <peter@silencethefoe.com>
2012-02-06 13:10:31 +0100

    Moved font-setting to user prefs. Added plugin to clear trailing space before save. Added hotkey for console.

---
+++ ClearTrailingSpacesOnSave.py
@@ -0,0 +1,6 @@
+import sublime, sublime_plugin
+
+class ClearTrailingSpacesOnSave(sublime_plugin.EventListener):
+
+    def on_pre_save(self, view):
+        view.run_command('delete_trailing_spaces')
